d99b018053157822074c15c199afd35957e82017
Mikhail Paulyshka <me@mixaill.tk>
2021-06-21 00:44:36 +0300

    data: parse end_rva from JSON

--- src_cpp/include/data/segment.h
+++ src_cpp/include/data/segment.h
@@ -11,9 +11,10 @@ namespace FakePDB::Data {
     struct Segment {
         std::string name;
         uint32_t start_rva;
+        uint32_t end_rva;
         std::string type;
         uint32_t selector;
     };
 
-    NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(Segment, name, start_rva, type, selector)
+    NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(Segment, name, start_rva, end_rva, type, selector)
 }
