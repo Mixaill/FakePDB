d1cc20689a62d2f86322128a9e3b9f13327627af
Mikhail Paulyshka <me@mixaill.tk>
2021-07-03 16:54:37 +0300

    data/general: add ability to get MachineType

--- src_cpp/include/data/general.h
+++ src_cpp/include/data/general.h
@@ -7,6 +7,9 @@
 //nlohmann
 #include "nlohmann/json.hpp"
 
+//llvm
+#include <llvm/Object/COFF.h>
+
 //fakepdb
 #include "data/label.h"
 
@@ -18,6 +21,26 @@ namespace FakePDB::Data {
 
         [[nodiscard]] bool empty() const{
             return filename.empty();
+        }
+
+        [[nodiscard]] llvm::COFF::MachineTypes getMachineType() const {
+            if(architecture == "x86" && bitness == 64) {
+                return llvm::COFF::IMAGE_FILE_MACHINE_AMD64;
+            }
+
+            if(architecture == "x86" && bitness == 32) {
+                return llvm::COFF::IMAGE_FILE_MACHINE_I386;
+            }
+
+            if(architecture == "arm" && bitness == 64) {
+                return llvm::COFF::IMAGE_FILE_MACHINE_ARM64;
+            }
+
+            if(architecture == "arm" && bitness == 32) {
+                return llvm::COFF::IMAGE_FILE_MACHINE_ARMNT;
+            }
+
+            return llvm::COFF::IMAGE_FILE_MACHINE_UNKNOWN;
         }
     };
 
