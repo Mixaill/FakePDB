ae3e68605500adc8414a2510f52f480b8ce157c3
Mikhail Paulyshka <me@mixaill.tk>
2021-08-21 21:30:23 +0300

    data: add ability to convert DB segment to LLVM COFF segment and vice versa

--- src_cpp/include/data/segment.h
+++ src_cpp/include/data/segment.h
@@ -8,15 +8,61 @@
 //nlohmann
 #include "nlohmann/json.hpp"
 
+//llvm
+#include <llvm/Object/COFF.h>
+
 namespace FakePDB::Data {
     struct Segment {
+        /**
+         * Segment alignment in bits
+         */
+        uint32_t align;
+
+        /**
+         * Number of bits in the segment addressing
+         */
+        uint32_t bitness;
+
+        /**
+         * Segment name
+         */
         std::string name;
-        uint32_t start_rva;
-        uint32_t end_rva;
-        std::string type;
+
+        /**
+         * Segment start address relative to image bases
+         */
+        uint32_t rva_start;
+      
+        /**
+         * Segment end address relative to image bases
+         */
+        uint32_t rva_end;
+
+        /**
+         * Segment permission
+         * 
+         * @note: possible value is combination of R,W,X
+         */
+        std::string permission;
+
+        /**
+         * Unique selector of segment
+         */
         uint32_t selector;
+
+        /**
+         * Type of the segment
+         * @note possible values: CODE , DATA
+         */
+        std::string type;
+
+        explicit Segment() = default;
+        explicit Segment(const llvm::object::coff_section& llvm_coff, uint32_t id);
+
+        llvm::object::coff_section toLLVM() const;
     };
-    NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(Segment, name, start_rva, end_rva, type, selector)
+
+    NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(Segment, align, bitness, name, rva_start, rva_end, permission, selector, type)
 
     class SegmentArray : public std::vector<Segment> {
     public:
